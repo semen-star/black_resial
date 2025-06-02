8c028a4178c66b9bd8ca9f474193dbcdffed5ff8
Семён Стариков <semenstarikov@MacBook-Pro-Semen.local>
2025-06-02 23:43:19 +0500

    add map and telegram

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-window.addEventListener('load', () => {
-            const logo = document.getElementById('logo');
-            const content = document.getElementById('main-content');
-            
-            // Показываем контент после анимации логотипа
-            setTimeout(() => {
-                content.style.display = 'block';
-            }, 2000);
-        });
+window.addEventListener("DOMContentLoaded", () => {
+  const logo = document.getElementById("logo");
+  const mainContent = document.getElementById("main-content");
+  const leftPanel = document.getElementById("left-panel");
+
+  // Показать логотип с анимацией
+  setTimeout(() => {
+    logo.classList.add("move-up");
+  }, 300);
+
+  // Показать контент
+  setTimeout(() => {
+    mainContent.style.opacity = "1";
+    leftPanel.classList.add("visible");
+  }, 2300); // после окончания анимации логотипа
+});
